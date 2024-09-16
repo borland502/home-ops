d6b3543d2bba8da2483838e37d498c0c2b9c70aa
Jeremy Hettenhouser <jhettenh@gmail.com>
2024-09-15 22:20:14 -0400

    Initial Commit

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,28 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import * as path from 'path';
+import { fileURLToPath } from 'node:url';
+import { log } from '@technohouser/log';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+console.log(log());
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to backend!' });
+});
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
