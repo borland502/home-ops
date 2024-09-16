40a5095ca4e208c9ad00028821b4f1fa10a1edfd
Jeremy Hettenhouser <jhettenh@gmail.com>
2024-09-15 22:55:58 -0400

    Implemented node config (toml)

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,14 +6,14 @@
 import express from 'express';
 import * as path from 'path';
 import { fileURLToPath } from 'node:url';
-import { log } from '@technohouser/log';
+import { warn } from '@technohouser/log';
+
+warn('hello there');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
-console.log(log());
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
