bc4914ce16eea9b44afa7ab96e706e13c9aa26c2
Jeremy Hettenhouser <jhettenh@gmail.com>
2025-02-15 12:57:47 -0500

    Refactored to use install doctor functions and homebrew for reliability

--- scripts/shell/bin/dasbootstrap.sh
+++ scripts/shell/bin/dasbootstrap.sh
@@ -1,96 +1,39 @@
 #!/usr/bin/env bash
 
-# dasbootstrap.sh assumes no previously installed elements and will attempt to use more common commands
-# to retrieve them
+set -e
+
+PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
+
+# shellcheck disable=SC1090,SC1091
+source "${HOME}/.local/share/automation/home-ops/scripts/shell/lib/install_doctor_functions.sh"
+
+ensureRootPackageInstalled sudo
+ensureRootPackageInstalled zip
+
+# shellcheck disable=SC1090,SC1091
+source "${HOME}/.local/share/automation/home-ops/scripts/shell/lib/logger.sh"
+
 
 declare -rx GH_RAW="https://raw.githubusercontent.com/borland502"
 declare -rx GH_PROJ="https://github.com/borland502/home-ops"
-# shellcheck disable=SC2155
-declare -rx HO_ROOT="$(mktemp -d)"
+
+create_sudo_user
+
+bootstrap_ansible_node
+
+ensureLocalPath "${XDG_BIN_HOME:-$HOME/.local/bin}"
+ensureLocalPath "${XDG_LIB_HOME:-$HOME/.local/lib}"
+ensureLocalPath "${XDG_DATA_HOME:-$HOME/.local/share}"
+ensureLocalPath "${XDG_CONFIG_HOME:-$HOME/.config}"
+ensureLocalPath "${XDG_CACHE_HOME:-$HOME/.cache}"
+ensureLocalPath "${XDG_STATE_HOME:-$HOME/.local/state}"
 
 # Fallback action in case the install is interrupted after root phase is done
 if [[ $USER == "root" ]] && [[ -f '/root/.rootfinished' ]]; then
-  # presume sudo powers at this point
-  _username="$(cat /root/.rootfinished)"
-  exec su - "${_username}" "/home/${_username}/$(basename "$0")"
-fi
-
-# @description Helper function for ensurePackageInstalled for Debian installations
-function ensureDebianPackageInstalled() {
-  if type sudo &>/dev/null && [ "$CAN_USE_SUDO" != 'false' ]; then
-    sudo apt-get update
-    sudo apt-get install -y "$1"
-  else
-    apt-get update
-    apt-get install -y "$1"
-  fi
-}
-
-# @description Helper function for ensurePackageInstalled for ArchLinux installations
-function ensureArchPackageInstalled() {
-  if type sudo &>/dev/null && [ "$CAN_USE_SUDO" != 'false' ]; then
-    sudo pacman update
-    sudo pacman -S "$1"
-  else
-    pacman update
-    pacman -S "$1"
-  fi
-}
-
-# @description Ensures given package is installed on a system.
-#
-# @arg $1 string The name of the package that must be present
-#
-# @exitcode 0 The package(s) were successfully installed
-# @exitcode 1+ If there was an error, the package needs to be installed manually, or if the OS is unsupported
-function ensurePackageInstalled() {
-  export CAN_USE_SUDO='true'
-  # TODO: Restore other types later
-  if ! [[ $(command -v "$1") ]]; then
-    ensureDebianPackageInstalled "$1"
-  elif [[ -f "/etc/arch-release" ]]; then
-    ensureArchPackageInstalled "$1"
-  fi
-}
-
-# @description If the user is running this script as root, then create a new user
-# and restart the script with that user. This is required because Homebrew
-# can only be invoked by non-root users.
-function create_sudo_user() {
-  local _username=${1:-'ansible'}
-
-  ensurePackageInstalled "sudo"
-  ensurePackageInstalled "zsh"
-  ensurePackageInstalled "zip"
-
-  if [ -z "$NO_INSTALL_HOMEBREW" ] && [ "$USER" == "root" ] && [ -z "$INIT_CWD" ] && type useradd &>/dev/null; then
-    # shellcheck disable=SC2016
-    logger info "Running as root - creating separate user named ${_username} to run script with"
-    echo "${_username} ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
-    useradd --create-home --shell "$(which zsh)" "${_username}" >/dev/null || ROOT_EXIT_CODE=$?
-    if [ -n "$ROOT_EXIT_CODE" ]; then
-      # shellcheck disable=SC2016
-      logger info "User ${_username} already exists"
-    fi
-
-    cp "$0" "/home/${_username}/$(basename "$0")"
-    chown "${_username}:${_username}" "/home/${_username}/$(basename "$0")"
-
-    mkdir -p /root/.ssh
-    chmod 700 /root/.ssh
-    read -rp "Enter the public key authorized for root access: " pubkey && echo "$pubkey" > \
-      "/root/.ssh/authorized_keys"
-    chmod 600 /root/.ssh/authorized_keys
-    chown "${_username}:${_username}" /root/.ssh/authorized_keys
-
-    chown -R "${_username}:${_username}" "/home/${_username}"
-
-    # shellcheck disable=SC2016
-    logger info "Reloading the script with the ${_username} user"
-    echo "${_username}" >/root/.rootfinished
+    # presume sudo powers at this point
+    _username="$(cat /root/.rootfinished)"
     exec su - "${_username}" "/home/${_username}/$(basename "$0")"
-  fi
-}
+fi
 
 ensurePackageInstalled build-essential
 ensurePackageInstalled git
@@ -98,17 +41,33 @@ ensurePackageInstalled curl
 ensurePackageInstalled rsync
 ensurePackageInstalled npm
 ensurePackageInstalled gcc
+ensurePackageInstalled unison
+ensurePackageInstalled jq
 
-if [[ $USER == "root" ]]; then
-  read -rp "Enter the username to create or default to ansible: " _user
 
-  if [ -z "$_user" ]; then
-    _user="ansible"
-  fi
+# if [[ $USER == "root" ]]; then
+#     read -rp "Enter the username to create or default to jhettenh: " _user
 
-  # Create a quick and dirty service user then restart the script as that user
-  create_sudo_user "${_user}"
+#     if [ -z "$_user" ]; then
+#         _user="jhettenh"
+#     fi
+
+#     # Create a quick and dirty service user then restart the script as that user
+#     create_sudo_user "${_user}"
+# fi
+
+ensureLocalPath "${HOME}/.local/share/automation/home-ops"
+if [[ ! -d "${HOME}/.local/share/automation/home-ops/.git" ]]; then
+    # Only clone if the directory doesn't already exist and have a .git directory
+    git clone --recurse-submodules https://github.com/borland502/home-ops.git "${HOME}/.local/share/automation/home-ops"
 fi
 
-mkdir -p "${HOME}/.local/share/automation"
-git clone --recurse-submodules https://github.com/borland502/home-ops.git "${HOME}/.local/share/automation/home-ops"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+brew install dasel
+
+if ! [[ $(command -v dasel) ]]; then
+    logger error "Dasel is required to parse toml"
+fi
+
+logger info "Bootstrapping complete"
